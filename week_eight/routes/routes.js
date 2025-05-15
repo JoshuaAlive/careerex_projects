3d2d841adb1b94c8ff552f2b105880bbef1168f5
JoshuaAlive <omotoshojtsen2020@futa.edu.ng>
2025-05-15 09:18:35 +0100

    Update routes.js to include basic healthcheck

--- week_eight/routes/routes.js
+++ week_eight/routes/routes.js
@@ -4,11 +4,15 @@ const {loginUser, registerUser} = require("../controllers/authController")
 const authMiddleware = require("../middleware/authMiddleware")
 const { getWallet, makeTransfer, getTransactions } = require("../controllers/walletController")
 
+const getStatus = (req, res) => {
+  res.status(200).json({"message": "PayFlow API operational!"})
+}
+
 
 router.post("/auth/register", registerUser)
 router.post("/auth/login", loginUser)
 router.get("/wallet", authMiddleware, getWallet)
 router.patch("/wallet/transfer", authMiddleware, makeTransfer)
 router.get("/transactions", authMiddleware, getTransactions)
-
+router.get("/", getStatus)
 module.exports = router
